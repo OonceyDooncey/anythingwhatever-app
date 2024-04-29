4286421ab9fdfcac75dc4e802c1f517349e8ee70
OonceyDooncey <sohenqi000@gmail.com>
2024-04-29 21:32:44 +0800

    [build]: Initialize Nuxt project

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <NuxtPage />
+    </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Permanent+Marker&family=Shadows+Into+Light+Two&display=swap");
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [],
+    theme: {
+        fontFamily: {
+            shadowintolight: ["Shadows Into Light Two", cursive],
+            caveat: ["Caveat", cursive],
+            permanentmarker: ["Permanent Marker", cursive],
+        },
+        colors: {
+            orange: "#E05F00",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
