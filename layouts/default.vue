c7399ae055d4c84a057f4d8929ab7182021c94bb
OonceyDooncey <sohenqi000@gmail.com>
2024-04-30 16:39:07 +0800

    [feat]: Added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-[url('/paper_background.png')] h-dvh w-screen bg-contain">
+        <h1
+            class="font-amatic text-white text-3xl font-bold tracking-wider p-3 h-[10%]">
+            ANYTHING, WHATEVER
+        </h1>
+        <slot />
+    </div>
+</template>
