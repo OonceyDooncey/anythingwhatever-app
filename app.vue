ebc0557e547e643ea88e20c5fa6588de80257835
OonceyDooncey <sohenqi000@gmail.com>
2024-04-30 16:38:44 +0800

    [style]: Moved font import to css file

--- app.vue
+++ app.vue
@@ -3,7 +3,3 @@
         <NuxtPage />
     </div>
 </template>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Permanent+Marker&family=Shadows+Into+Light+Two&display=swap");
-</style>
