ca71d8d1f646b3617e0fdd4b8d55d7e50617dd07
OonceyDooncey <sohenqi000@gmail.com>
2024-05-02 15:37:12 +0800

    [misc]: Linked to Cloudinary

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "shadcn-nuxt"],
+    image: {
+        cloudinary: {
+            baseURL: "https://res.cloudinary.com/dfdaqvqps/image/fetch/",
+        },
+    },
     ssr: false,
 });
