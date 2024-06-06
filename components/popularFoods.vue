0b3895bc65d1d2264eb18b489c8fc4a101c7c397
OonceyDooncey <sohenqi000@gmail.com>
2024-06-06 16:45:11 +0800

    [refractor]: Use nuxt lazy fetch instead of $fetch

--- components/popularFoods.vue
+++ components/popularFoods.vue
@@ -1,107 +1,64 @@
 <template>
-    <div class="mx-auto w-[80%] mt-2">
-        <h3 class="font-caveat font-bold text-lead xs:text-2xl text-3xl">
-            Popular choice
-        </h3>
-        <div>
-            <ul ref="skeleton" class="mt-3">
-                <li
-                    class="w-full h-[100px] border-2 border-lead rounded-xl flex items-center justify-between p-4 mt-3">
-                    <Skeleton
-                        class="w-[62px] h-[62px] rounded-xl mr-2 bg-lead50" />
-                    <div class="flex flex-col w-4/6 items-start">
-                        <Skeleton
-                            class="w-[100px] h-[25px] rounded-xl xs:w-[80px]" />
-                        <div class="mt-[7px] flex items-center">
-                            <Skeleton
-                                class="w-[65px] xs:w-[55px] h-[32px] mr-1" />
-                            <Skeleton class="w-[65px] xs:w-[55px] h-[32px]" />
-                        </div>
-                    </div>
-                    <span class="w-1/6 flex justify-center">
-                        <Skeleton
-                            class="w-[20px] h-[20px] rounded-sm sm:w-[15px] xs:w-[15px]" />
-                    </span>
-                </li>
-                <li
-                    class="w-full h-[100px] border-2 border-lead rounded-xl flex items-center justify-between p-4 mt-3">
-                    <Skeleton
-                        class="w-[62px] h-[62px] rounded-xl mr-2 bg-lead50" />
-                    <div class="flex flex-col w-4/6 items-start">
-                        <Skeleton
-                            class="w-[100px] h-[25px] rounded-xl xs:w-[80px]" />
-                        <div class="mt-[7px] flex items-center">
-                            <Skeleton
-                                class="w-[65px] xs:w-[55px] h-[32px] mr-1" />
-                            <Skeleton class="w-[65px] xs:w-[55px] h-[32px]" />
-                        </div>
-                    </div>
-                    <span class="w-1/6 flex justify-center">
-                        <Skeleton
-                            class="w-[20px] h-[20px] rounded-sm sm:w-[15px] xs:w-[15px]" />
-                    </span>
-                </li>
-                <li
-                    class="w-full h-[100px] border-2 border-lead rounded-xl flex items-center justify-between p-4 mt-3">
-                    <Skeleton
-                        class="w-[62px] h-[62px] rounded-xl mr-2 bg-lead50" />
-                    <div class="flex flex-col w-4/6 items-start">
-                        <Skeleton
-                            class="w-[100px] h-[25px] rounded-xl xs:w-[80px]" />
-                        <div class="mt-[7px] flex items-center">
-                            <Skeleton
-                                class="w-[65px] xs:w-[55px] h-[32px] mr-1" />
-                            <Skeleton class="w-[65px] xs:w-[55px] h-[32px]" />
-                        </div>
-                    </div>
-                    <span class="w-1/6 flex justify-center">
-                        <Skeleton
-                            class="w-[20px] h-[20px] rounded-sm sm:w-[15px] xs:w-[15px]" />
-                    </span>
-                </li>
-            </ul>
-            <ul v-for="food in popularFoods">
-                <li class="mt-3">
-                    <Drawer>
-                        <DrawerTrigger
-                            @click="
-                                fetchFoodInfo(food.food);
-                                store.updatePopularity = true;
-                            "
-                            class="bg-transparent border-2 border-lead rounded-xl flex items-center p-4 w-full justify-between">
-                            <NuxtImg
-                                :src="food.image"
-                                format="webp"
-                                width="60px"
-                                height="60px"
-                                class="rounded-xl mr-2 w-[60px] h-[60px] object-cover xs:w-[50px] xs:h-[50px]" />
-                            <div class="flex flex-col w-4/6">
-                                <h4
-                                    class="text-left font-shadowsintolight text-lead text-2xl">
-                                    {{ food.food }}
-                                </h4>
-                                <div class="flex">
-                                    <Badge
-                                        class="bg-orange border-0 mr-1 font-caveat text-lg hover:bg-orange pl-4 pr-4 xs:pl-2 xs:pr-2">
-                                        {{ food.calories }}kcal
-                                    </Badge>
-                                    <Badge
-                                        class="bg-orange border-0 font-caveat text-lg hover:bg-orange pl-4 pr-4 xs:pl-2 xs:pr-2">
-                                        {{ food.cuisine }}
-                                    </Badge>
-                                </div>
-                            </div>
-                            <span
-                                class="material-symbols-outlined text-lead w-1/6">
-                                chevron_right
-                            </span>
-                            <FoodInfo />
-                        </DrawerTrigger>
-                    </Drawer>
-                </li>
-            </ul>
-        </div>
+  <div class="mx-auto w-[80%] mt-2">
+    <h3 class="font-caveat font-bold text-lead xs:text-2xl text-3xl">Popular choice</h3>
+    <div>
+      <ul class="mt-3" v-if="pending">
+        <li
+          class="w-full h-[100px] border-2 border-lead rounded-xl flex items-center justify-between p-4 mt-3"
+          v-for="skeleton in 3"
+          :key="skeleton"
+        >
+          <Skeleton class="w-[62px] h-[62px] rounded-xl mr-2 bg-lead50" />
+          <div class="flex flex-col w-4/6 items-start">
+            <Skeleton class="w-[100px] h-[25px] rounded-xl xs:w-[80px]" />
+            <div class="mt-[7px] flex items-center">
+              <Skeleton class="w-[65px] xs:w-[55px] h-[32px] mr-1" />
+              <Skeleton class="w-[65px] xs:w-[55px] h-[32px]" />
+            </div>
+          </div>
+          <span class="w-1/6 flex justify-center">
+            <Skeleton class="w-[20px] h-[20px] rounded-sm sm:w-[15px] xs:w-[15px]" />
+          </span>
+        </li>
+      </ul>
+      <ul v-for="food in popularFoods" v-else>
+        <li class="mt-3">
+          <Drawer>
+            <DrawerTrigger
+              @click="
+                fetchFoodInfo(food.food);
+                store.updatePopularity = true;
+              "
+              class="bg-transparent border-2 border-lead rounded-xl flex items-center p-4 w-full justify-between"
+            >
+              <NuxtImg
+                :src="food.image"
+                format="webp"
+                width="60px"
+                height="60px"
+                class="rounded-xl mr-2 w-[60px] h-[60px] object-cover xs:w-[50px] xs:h-[50px]"
+              />
+              <div class="flex flex-col w-4/6">
+                <h4 class="text-left font-shadowsintolight text-lead text-2xl">
+                  {{ food.food }}
+                </h4>
+                <div class="flex">
+                  <Badge class="bg-orange border-0 mr-1 font-caveat text-lg hover:bg-orange pl-4 pr-4 xs:pl-2 xs:pr-2">
+                    {{ food.calories }}kcal
+                  </Badge>
+                  <Badge class="bg-orange border-0 font-caveat text-lg hover:bg-orange pl-4 pr-4 xs:pl-2 xs:pr-2">
+                    {{ food.cuisine }}
+                  </Badge>
+                </div>
+              </div>
+              <span class="material-symbols-outlined text-lead w-1/6"> chevron_right </span>
+              <FoodInfo />
+            </DrawerTrigger>
+          </Drawer>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -110,36 +67,22 @@ import { Drawer, DrawerTrigger } from "@/components/ui/drawer";
 import { Skeleton } from "@/components/ui/skeleton";
 
 useHead({
-    link: [
-        {
-            rel: "stylesheet",
-            href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,0",
-        },
-    ],
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,0",
+    },
+  ],
 });
 
-const skeleton = ref();
-const popularFoods = ref();
 const store = useGlobalStore();
 
-const fetchPopularFoods = async function () {
-    const response = await $fetch("/api/popularfoods");
+const { data: popularFoods, pending, error } = await useLazyFetch("/api/popularfoods");
 
-    if (response.statusCode !== 200) {
-        showError({
-            statusCode: response.statusCode,
-            statusMessage: response.message,
-        });
-    }
-
-    if (response.statusCode === 200) {
-        skeleton.value.classList.add("hidden");
-    }
-
-    popularFoods.value = response.popularFoodList;
-};
-
-onMounted(() => {
-    fetchPopularFoods();
-});
+if (error.value) {
+  showError({
+    statusCode: 404,
+  });
+  console.log(error);
+}
 </script>
--- server/api/popularfoods.get.ts
+++ server/api/popularfoods.get.ts
@@ -1,46 +1,25 @@
 import { supabase } from "../dbAuth/supabase";
 
-interface popularFood {
-    food: string;
-    popularity: number;
-    calories: number;
-    cuisine: string;
-    image: string;
-}
-
-interface popularFoodResponse {
-    popularFoodList: popularFood[];
-    statusCode: number;
-    message: string;
-}
+type popularFood = {
+  food: string;
+  popularity: number;
+  calories: number;
+  cuisine: string;
+  image: string;
+};
 
 export default defineEventHandler(async () => {
-    let response: popularFoodResponse;
-    let sortedList: popularFood[] = [];
+  let response: popularFood[] = [];
 
-    const { data, error } = await supabase
-        .from("foods")
-        .select("food, popularity, calories, cuisine, image");
+  const { data, error } = await supabase.from("foods").select("food, popularity, calories, cuisine, image");
 
-    if (error) {
-        response = {
-            popularFoodList: [],
-            statusCode: 500,
-            message: "Failed to retrieve data",
-        };
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    if (data) {
-        sortedList = data
-            .sort((a, b) => b.popularity - a.popularity)
-            .slice(0, 3);
-    }
+  if (data) {
+    response = data.sort((a, b) => b.popularity - a.popularity).slice(0, 3);
+  }
 
-    response = {
-        popularFoodList: sortedList,
-        statusCode: 200,
-        message: "Success",
-    };
-
-    return response;
+  return response;
 });
