e9cf972d9d4f52428dbf2c9fb80d30a5df894c30
OonceyDooncey <sohenqi000@gmail.com>
2024-06-06 16:41:36 +0800

    [fix]: Relocated useSeoMeta to fix vue head error

--- app.vue
+++ app.vue
@@ -4,21 +4,3 @@
         <NuxtPage />
     </div>
 </template>
-
-<script>
-useSeoMeta({
-    title: "Anything, Whatever",
-    ogTitle: "Anything, Whatever",
-    description:
-        "A food randomiser for people suffering decision fatigue. Generate food suggestions base on your preference.",
-    ogDescription:
-        "A food randomiser for people suffering decision fatigue. Generate food suggestions base on your preference.",
-    ogImage: "/ogImage.png",
-    twitterCard: "summary_large_image",
-    twitterTitle: "Anything, whatever",
-    twitterDescription:
-        "A food randomiser for people suffering decision fatigue. Generate food suggestions base on your preference.",
-    twitterImage: "/ogImage.png",
-    ogUrl: "https://anythingwhatever-app.vercel.app/",
-});
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -93,6 +93,22 @@ useHead({
     ],
 });
 
+useSeoMeta({
+    title: "Anything, Whatever",
+    ogTitle: "Anything, Whatever",
+    description:
+        "A food randomiser for people suffering decision fatigue. Generate food suggestions base on your preference.",
+    ogDescription:
+        "A food randomiser for people suffering decision fatigue. Generate food suggestions base on your preference.",
+    ogImage: "/ogImage.png",
+    twitterCard: "summary_large_image",
+    twitterTitle: "Anything, whatever",
+    twitterDescription:
+        "A food randomiser for people suffering decision fatigue. Generate food suggestions base on your preference.",
+    twitterImage: "/ogImage.png",
+    ogUrl: "https://anythingwhatever-app.vercel.app/",
+});
+
 const store = useGlobalStore();
 
 onMounted(() => {
